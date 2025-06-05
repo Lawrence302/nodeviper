b2808fd6e1faf3b44f9eba83f4261df9db071ca6
Lawrence <ganjeblerence@gmail.com>
2025-06-06 00:46:46 +0530

    commented cod in adminRoutes.js and changed the app name in index.html from snakegame to NodeViper

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import { deleteUser, getUsers } from '../controller/adminController.js';
+// import { deleteUser, getUsers } from '../controller/adminController.js';
 
 const router = express.Router();
 
-router.get('/',getUsers);
-router.delete('/user/:user_id', deleteUser)
+// router.get('/',getUsers);
+// router.delete('/user/:user_id', deleteUser)
 
 export default router;
