a56d3c44ffb26548555ac1284e7c32385496b931
Lawrence <ganjeblerence@gmail.com>
2025-06-06 15:06:13 +0530

    updated the db.js file to contain the renderdb url

--- backend/model/db.js
+++ backend/model/db.js
@@ -6,14 +6,19 @@ dotenv.config()
 
 const {Pool} = pkg
 // connecting to posgres db
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // often needed for cloud hosted DBs
+  }
 });
-
 // Test the connection
 
 const connectdb = () => {
